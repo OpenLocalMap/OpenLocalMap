2bcf0df5476d6b9d41ddd7d7facc096200da7246
OpenLocalMap <bencalnan@hotmail.com>
2014-11-25 09:22:43 +0000

    Update variableSetup.js

--- PHP Version/js/variableSetup.js
+++ PHP Version/js/variableSetup.js
@@ -1,10 +1,10 @@
 ///Globals ///////
-var wmsURL = '';
-var boundaryName = "";
-var boundaryWMSname = "";
+var wmsURL = '';  //WMS connection string e.g. 
+var boundaryName = ""; //Name for Boundary (Can be anything)  - To be made optional
+var boundaryWMSname = ""; //Name of Boundary WMS Name (has to be specific)
 
 var serverPath = "";
 var kmllink = serverPath + "wms/kml?mode=download&layers=";
 var csvlink = serverPath + "wfs?service=wfs&version=2.0.0&request=GetFeature&outputFormat=csv&typeNames=";
-var pollutionDialogString = "";
+var pollutionDialogString = ""; //String for Dialog box (to be moved into metadata)
 
