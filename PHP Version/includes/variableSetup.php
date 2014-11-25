a4665091e0d7dcc43a579a929c8089e71a0661f2
OpenLocalMap <bencalnan@hotmail.com>
2014-11-25 09:21:09 +0000

    Update variableSetup.php

--- PHP Version/includes/variableSetup.php
+++ PHP Version/includes/variableSetup.php
@@ -1,12 +1,12 @@
 <?php
 
-define("metaschema", "");
-define("metatable", "");
-define("wmslegendURL" ,""); 
-define("LLPGtable", "");
-define("metaDBConnectionStr", "");
-define("metaUser", "");
-define("metaPass", "");
-define("llpgDBConnectionStr", "");
-define("llpgUser", "");
-define("llpgPass", "");
+define("metaschema", ""); //Schema in which metadata table is stored for Oracle Databases
+define("metatable", "");//Name of metadata table
+define("wmslegendURL" ,""); //WMS connection path for legend graphics
+define("LLPGtable", ""); //Adddress search table , not applicable at present
+define("metaDBConnectionStr", ""); //Connection string for metadata database
+define("metaUser", ""); //Username for metadata database
+define("metaPass", ""); //Password for metadata database
+define("llpgDBConnectionStr", ""); //Connection string for address database, not applicable at present
+define("llpgUser", ""); //Username for address database, not applicable at present
+define("llpgPass", ""); //Password for address database, not applicable at present
