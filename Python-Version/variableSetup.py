4988aa90e70ca6d24df8ddb22e481d02889ea41c
OpenLocalMap <bencalnan@hotmail.com>
2014-12-31 14:16:25 +0000

    Create variableSetup.py
    Python version still work to be done

---
+++ Python-Version/variableSetup.py
@@ -0,0 +1,28 @@
+#    OpenLocalMap OpenSource web mapping for local government
+#    Copyright (C) <2014>  <Ben Calnan>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+metaschema =  "" ##Schema in which metadata table is stored for Oracle Databases
+metatable =  ""##Name of metadata table
+wmslegendURL = "" ##WMS connection path for legend graphics
+LLPGtable = "" ##Adddress search table , not applicable at present
+metaDBConnectionStr = "" ##Connection string for metadata database
+metaUser = "" ##Username for metadata database
+metaPass =  "" ##Password for metadata database
+llpgDBConnectionStr =  "" ##Connection string for address database, not applicable at present
+llpgUser = "" ##Username for address database, not applicable at present
+llpgPass = "" ##Password for address database, not applicable at present
